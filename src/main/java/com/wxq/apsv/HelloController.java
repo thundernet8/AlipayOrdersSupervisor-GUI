84ff17311eb3fae085ec577aa17535f3f02a2111
吴学谦 <wuxueqian@ezbuy.com>
2017-08-09 19:15:04 +0800

    setup maven

---
+++ src/main/java/com/wxq/apsv/HelloController.java
@@ -0,0 +1,46 @@
+package com.wxq.apsv;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloController
+{
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
+    @FXML private TextField firstNameField;
+    @FXML private TextField lastNameField;
+    @FXML private Label messageLabel;
+
+    public void sayHello() {
+
+        String firstName = firstNameField.getText();
+        String lastName = lastNameField.getText();
+
+        StringBuilder builder = new StringBuilder();
+
+        if (!StringUtils.isEmpty(firstName)) {
+            builder.append(firstName);
+        }
+
+        if (!StringUtils.isEmpty(lastName)) {
+            if (builder.length() > 0) {
+                builder.append(" ");
+            }
+            builder.append(lastName);
+        }
+
+        if (builder.length() > 0) {
+            String name = builder.toString();
+            log.debug("Saying hello to " + name);
+            messageLabel.setText("Hello " + name);
+        } else {
+            log.debug("Neither first name nor last name was set, saying hello to anonymous person");
+            messageLabel.setText("Hello mysterious person");
+        }
+    }
+
+}
