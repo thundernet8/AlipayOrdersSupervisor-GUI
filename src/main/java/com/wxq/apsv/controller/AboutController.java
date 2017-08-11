29fa53f933c760208ff6455508a9de3e7a38f3ae
吴学谦 <wuxueqian@ezbuy.com>
2017-08-11 20:50:34 +0800

    add about page labels

--- src/main/java/com/wxq/apsv/controller/AboutController.java
+++ src/main/java/com/wxq/apsv/controller/AboutController.java
@@ -6,6 +6,15 @@ import com.wxq.apsv.enums.*;
 import com.wxq.apsv.interfaces.*;
 
 public class AboutController extends JPanel implements TabController {
+    private JLabel logoLabel;
+
+    private JLabel titleLabel;
+
+    private JLabel copyrightLabel;
+
+    private JLabel versionLabel;
+
+    private JLabel linkLabel;
 
     @Override
     public WinTab getTabType() {
@@ -13,6 +22,15 @@ public class AboutController extends JPanel implements TabController {
     }
 
     public AboutController() {
+        this.InitViews();
+        this.InitListeners();
+    }
+
+    private void InitViews() {
+
+    }
+
+    private void InitListeners() {
 
     }
 }
