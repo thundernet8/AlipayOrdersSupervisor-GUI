3308d5cfbd0f3e847ba195fdfa61bdbbe59e70e2
吴学谦 <wuxueqian@ezbuy.com>
2017-08-15 17:20:13 +0800

    save

---
+++ src/main/java/com/wxq/apsv/worker/PushManager.java
@@ -0,0 +1,17 @@
+package com.wxq.apsv.worker;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+
+public final class PushManager {
+    private static Map<String, Timer> timerMap = new HashMap<>();
+
+    public static void AddTimer(Timer timer, int taskId) {
+        timerMap.put(Integer.toString(taskId), timer);
+    }
+
+    public static Timer GetTimer(int taskId) {
+        return timerMap.get(Integer.toString(taskId));
+    }
+}
