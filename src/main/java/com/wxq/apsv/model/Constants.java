a95b7cc71338943bcbc14063ab931f756edb3184
wuxueqian <wuxueqian2010@hotmail.com>
2017-08-17 01:59:10 +0800

    correct name

--- src/main/java/com/wxq/apsv/model/Constants.java
+++ src/main/java/com/wxq/apsv/model/Constants.java
@@ -1,7 +1,7 @@
 package com.wxq.apsv.model;
 
 public class Constants {
-    public static final String APP_NAME = "AlipaySupervisor GUI";
+    public static final String APP_NAME = "AlipayOrdersSupervisor GUI";
 
     public static final String APP_VERSION = "1.0.0";
 
